a46179848f36afcd2750b4dcdf7ec49fbbf15b8a
harshv5094 <harshv5094@gmail.com>
2024-12-19 13:53:19 +0530

    feat(main): explain main function in c

---
+++ basics/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * HACK: Your Program's entry point is a function called "main".
+ * The return type can be anything, however most operating systems expect a
+ * return type can be anything, however most operating systems expect a return
+ * type of 'int' for error coding processing
+ */
+int main(void) {
+  printf("Hello from the main function");
+
+  /*
+   * NOTE: Here we are returning the exit code 0 to tell the Operating System
+   * that the program execution is successful.
+   */
+  return 0;
+}
